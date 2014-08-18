a9f3b45841d729ab9742254db55c30f409ff5362
enham <enham@redhat.com>
2014-08-18 16:09:20 -0400

    Update TicketEndpoint.java

--- src/main/java/org/jboss/jdf/example/ticketmonster/rest/TicketEndpoint.java
+++ src/main/java/org/jboss/jdf/example/ticketmonster/rest/TicketEndpoint.java
@@ -25,15 +25,6 @@ public class TicketEndpoint
    @PersistenceContext(unitName="primary")
    private EntityManager em;
 
-   @POST
-   @Consumes("application/json")
-   public Response create(TicketDTO dto)
-   {
-      Ticket entity = dto.fromDTO(null, em);
-      em.persist(entity);
-      return Response.created(UriBuilder.fromResource(TicketEndpoint.class).path(String.valueOf(entity.getId())).build()).build();
-   }
-
    @DELETE
    @Path("/{id:[0-9][0-9]*}")
    public Response deleteById(@PathParam("id") Long id)
